fae12e511f69257da9df5cb76ad40531b5cf3213
000hen <git@muisnowdevs.one>
2024-02-05 11:44:00 +0800

    [bugfix] Fix Garbage Trunk system message misleading

--- src/main/java/one/muisnowdevs/minecraft/allinone/events/ItemGarbageTruck.java
+++ src/main/java/one/muisnowdevs/minecraft/allinone/events/ItemGarbageTruck.java
@@ -6,59 +6,64 @@ import one.muisnowdevs.minecraft.allinone.AllInOne;
 import one.muisnowdevs.minecraft.allinone.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 
 import java.util.List;
 
+enum Times {
+    TEN_MINUTE,
+    ONE_MINUTE
+}
+
 public class ItemGarbageTruck {
     private final AllInOne _plugin;
-
-    private Boolean isClearing = false;
-    private Integer clearIndex = 0;
 
     public ItemGarbageTruck(AllInOne plugin) {
         _plugin = plugin;
 
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(_plugin, this::clearEvent, 0L, 12000L);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(_plugin, () -> notifyEvent(Times.TEN_MINUTE), 12000L, 24000L);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(_plugin, () -> notifyEvent(Times.ONE_MINUTE), 22800L, 24000L);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(_plugin, this::clearEvent, 24000L, 24000L);
     }
 
-    private void notifyEvent() {
+    private void notifyEvent(Times time) {
+        String timeString = null;
+
+        switch (time) {
+            case ONE_MINUTE:
+                timeString = "1";
+                break;
+
+            case TEN_MINUTE:
+                timeString = "10";
+                break;
+        }
+
         _plugin.getServer().sendMessage(Component.text()
                 .append(Utils.titleTag("垃圾車", NamedTextColor.AQUA))
-                .append(Component.text("即將在 10 分鐘後回收掉落物！"))
+                .append(Component.text(String.format("即將在 %s 分鐘後回收掉落物！", timeString)))
                 .build());
     }
 
     private void clearEvent() {
-        if (isClearing) return;
-        if (clearIndex++ == 0) {
-            notifyEvent();
-            return;
-        }
-
-        isClearing = true;
-
         _plugin.getServer().sendMessage(Component.text()
                 .append(Utils.titleTag("垃圾車", NamedTextColor.AQUA))
                 .append(Component.text("正在從所有世界回收掉落物..."))
                 .build());
 
-        final int[] entityCount = {0};
+        final long[] entityCount = {0};
 
         List<World> world = _plugin.getServer().getWorlds();
         world.forEach(wd -> {
-            List<Entity> entities = wd.getEntities();
-            entityCount[0] = entityCount[0] + entities.size();
-            entities.stream().filter(Item.class::isInstance).forEach(Entity::remove);
+            wd.getEntities().stream().filter(Item.class::isInstance).forEach(entity -> {
+                entityCount[0]++;
+                entity.remove();
+            });
         });
 
         _plugin.getServer().sendMessage(Component.text()
                 .append(Utils.titleTag("垃圾車", NamedTextColor.AQUA))
                 .append(Component.text(String.format("回收成功 %o 件垃圾！世界變得一片祥和了！", entityCount[0])))
                 .build());
-
-        isClearing = false;
-        clearIndex = 0;
     }
 }
